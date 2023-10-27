93e5cb22c3dadcbca8676a49bcbecc76340b8b1c
A01754650 <88682624+A01754650@users.noreply.github.com>
2023-10-26 21:49:34 -0600

    V4

--- myproject/backend.py
+++ myproject/backend.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
-
+from werkzeug.utils import secure_filename
 from flask_cors import CORS
 import joblib 
+import os
+import csv
 
 dt = joblib.load("./static/dt.joblib")
 
@@ -24,5 +26,36 @@ def predict_json():
     print(y_pred)
     return jsonify({"result": y_pred[0]})
 
+@app.route("/predict_form", methods=["POST"])
+def predict_form():
+    data = request.form
+    X = [
+        float (data["pH"]),
+        float (data["sulphates"]),
+        float (data["alcohol"])
+    ]
+    y_pred = dt.predict(X)
+    print(y_pred)
+    return jsonify({"result": y_pred[0]})
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug = False, port = 8081)
+    
+@app.route("/predict_file", methods = ["POST"])
+def predict_file():
+    files = request.files["archivo"]
+    filename = secure_filename(file.filename)
+    #file.save(f"./static/{filename}")
+    path = os.path.join(os.getcwd(),"static", filename)
+    file.save(path)
+    with open(path,"r") as f:
+        reader = csv.DictReader(f)
+        X = [[float(row[0]), float(row[1]),float(row[2])]]
+        y_pred = dt.predict(X)
+        return jsonify({
+            "results": [{"key": f"{k}",
+                         "results": f"{v}"} for k, v in enumerate(y_pred)]
+        })
+    
+    
+    
